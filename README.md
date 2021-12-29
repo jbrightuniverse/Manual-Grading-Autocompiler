56aa548d487aafd969b74aed7404bb704001a449
James Yu <30967260+jbrightuniverse@users.noreply.github.com>
2021-12-29 15:03:44 -0800

    initial files

---
+++ README.md
@@ -0,0 +1,126 @@
+# Manual Grading Autocompiler
+
+James Yuming Yu, 2021
+
+github: jbrightuniverse
+
+This is a Python utility for converting general manual grading data from PrairieLearn into CSV files that TAs can use to grade and directly upload to PrairieLearn.
+
+It is recommended to use this utility on a course account through department servers as this ensures security of student data and provides a central location for TAs to access grading files.
+
+## Step 1: Insert Python script into remote server
+
+Copy `autocompile.py` into the server under the folder `YEAR/grading`. This should look something like:
+
+![folder tree](readme_images/tree_1.png)
+
+Inside the script, at about line 20, is a line of code containing the name of the course on PrairieLearn. Whenever starting over in a new term, change this line to have the new course name (e.g. CPSC_213_2021W2).
+
+## Step 2: Create a folder for the assignment of the form "AX"
+
+Long-term, your folder structure should look like this. Assignment folders must use this exact formatting with a capital A followed by the assignment number.
+
+![folder tree](readme_images/tree_2.png)
+
+## Step 3: Insert a JSON rubric into the assignment folder
+
+This rubric should be located on the department GitHub repository.
+
+## Step 4: Decide whether you need to grade all submissions or just ones with manual grading
+
+For example, if your assignment uses autograders, you only need manual grading, and so you don't need to change anything.
+
+However, if your assignment has autograders that only check for compilation and do not check for correctness (e.g. CPSC 213 A9), you need to regrade all submissions. In that case, open `autocompile.py` and set the following flag to `True` (by default it should be `False`):
+
+![the flag](readme_images/flag.png)
+
+## Step 5: Collect the right CSV file to grade from
+
+On PrairieLearn, click on the assignment you want to grade, and then click downloads at the top of the screen:
+
+![downloads](readme_images/downloads_1.png)
+
+IF YOU ARE ONLY GRADING SUBMISSIONS THAT NEED MANUAL GRADING (e.g. not 213 A9):
+
+then download `submissions_for_manual_grading.csv` and put it in the assignment folder you put the rubric in.
+
+OTHERWISE, IF YOU NEED TO REGRADE ALL SUBMISSIONS OF THE ASSIGNMENT (e.g. 213 A9):
+
+then download `final_submissions.csv` and put it in the assignment folder you put the rubric in.
+
+## Step 6: Collect the right set of files
+
+Still on the downloads page, if you are ONLY GRADING SUBMISSIONS THAT NEED MANUAL GRADING (e.g. not A9), download `files_for_manual_grading.zip`, open it, and move the folder inside (it should say `AssignmentX` where X is the assignment) to the same location in the remote server where the rubric and CSV are.
+
+IF OTHERWISE YOU ARE REGRADING ALL SUBMISSIONS (e.g. A9), download `final_files.zip`, open it, and move the folder inside to the same location in the remote server where the rubric and CSV are.
+
+If using the first option (not A9), your folder structure under the assignment folder should now look similar to this:
+
+![folder tree](readme_images/tree_3.png)
+
+If using the second option (A9), the only difference is `AssignmentX` comes from the `final_files` zip file, and the CSV file at the bottom should be `final_submissons.csv`.
+
+Notice that `autocompile.py` is not inside the `AX` folder here: as in Step 2, it should be ONE LEVEL OUTSIDE THE `AX` FOLDERS.
+
+## Step 7: Run the script in the terminal
+
+`cd` to the top-level directory containing `autocompile.py`. This should be the `grading` folder following conventions above.
+
+Inside that folder, run `python3 autocompile.py X` where you replace `X` with the assignment number. For example, if you want to grade A10, type `python3 autocompile.py 10`.
+
+The program will then compile necessary data.
+
+## Step 8: Open the resulting folder and grade
+
+For example, after running A10, you might get this:
+
+![folder tree](readme_images/tree_4.png)
+
+There is a separate folder for each subgroup of questions (e.g. if questions from the A10 and A11 bank were used to build A10). Inside each question subfolder is the list of students or groups, within which are relevant files. At the bottom of each folder will be two files, which may look like:
+
+![folder tree](readme_images/tree_5.png)
+
+The first one is a TXT file containing a concatenated list of every submission made by a student or group for that question. This can make manual grading faster by not having to open each individual folder; if you want to view source files, you can still do so by opening the student-specific folder inside each question folder.
+
+The second one is a CSV file containing the actual grade entry points. The list of keys at the top might look like:
+
+![folder tree](readme_images/csv.png)
+
+Under the `points` column is an empty cell in front of each groupname/student. This is where you would enter the number of points the student would be graded for each question. If you want to grade in percentages instead, you can change the column name to `score_perc` and then enter percents as a whole number (e.g. to enter 100%, type 100).
+
+On the right hand side of each row is a bit of HTML text representing the rubric for the question. To add your own feedback to the rubric, simply continue typing at the end of the existing text. **DO NOT DELETE THE EXISTING TEXT**, as rubrics are not auto-generated without them. Writing text after will work. If you wish to add a newline, note that this is an HTML entry field, so use `</br>` to make a line break.
+
+## Step 9: Upload back to PrairieLearn
+
+When you are finished grading, you can directly download the CSV file you were grading on and upload it to PrairieLearn. On the assignment tab you were using, click on `Uploads`, and then `Upload new question scores`. **DO NOT CLICK THE OTHER UPLOAD BUTTON**, as this modifies assignment-wide scores.
+
+![pl](readme_images/pl.png)
+
+Simply upload the CSV and hit `Upload`. The grades and feedback will then be automatically applied.
+
+## DEBUGGING: Broken Zips
+
+In the event that opening a zip file produces the error of the form "cannot open" or "invalid folder" or similar unopenable errors, this means a student has created a group name. For this, you will need to first save the zip file to your computer and identify on PrairieLearn what the group name's name is.
+
+Then, in the same folder as the zip file, run:
+`zip -d filename.zip 'first_few_chars_of_groupname*'`
+
+You can either use `'first_few_chars_of_groupname*'` (including asterisk at the end, e.g. `'the_start_of_a_very_long_group_name_tha*'`) which will match the beginning of the long group name (so you do not need to type it yourself).
+
+Or you can directly enter `'the entire groupname'` (no asterisk here) where you replace that text with the entire group name (e.g. `'my_fun_group_name'`).
+
+After doing this, the zip file will no longer contain the submissions of that long group name. These will need to be manually downloaded from PrairieLearn and manually graded separately.
+
+You will also need to delete the corresponding entries from the manual grading CSV. Simply delete the rows before launching the grading utility.
+
+## Debugging: ERROR: No such file or directory
+
+If the Python script raises a file or folder not found error, you may have the grading mode set incorrectly via the flag at the top of the file. Make sure to use the "final submissions" and "final files" data if grading everyone's work, and only the "for manual grading" data if only grading non-autograded work.
+
+Also make sure the filenames are exactly as given in PrairieLearn for the downloadable CSV and submission folders. The script does not support alternate names at the moment.
+
+## CONTACT
+
+If you encounter any issues that have not already been outlined here, simply raise an issue in this repository.
+
+This code is also open-source, so feel free to create a pull request.
---
+++ autocompile.py
@@ -0,0 +1,161 @@
+"""
+
+A script to convert PrairieLearn's manual grading files to grader-friendly targeted CSVs.
+See README.md for instructions on how to set up the folderstructure.
+
+Originally written by James Yuming Yu, 2021
+github: jbrightuniverse
+Released under the MIT License.
+
+"""
+
+import csv
+from collections import defaultdict
+
+from shutil import copyfile
+import os
+import sys
+import json
+
+COURSE_NAME = "CPSC_213_2021W1" # the name of the course on PrairieLearn
+MAX_ASSIGNMENT = 11 # maximum assignment number, just as a bounds check
+GRADE_ALL_SUBMISSIONS = True # change this to True if you wish to grade all submissions rather than only the ones that have not been autograded
+# relevant for assignments where the autograder only checks for completion and not correctness
+
+
+# verify that an assignment number was given on the command line
+# should just be a number, no "A"
+try:
+    assignment = sys.argv[1]
+except:
+    print("USAGE: python3 autocompile.py <assignment_number>")
+    sys.exit()
+
+# assignment should be an integer
+try:
+    assignment = int(assignment)
+except:
+    print("Assignment must be an integer!")
+    sys.exit()
+
+# assignment should be valid
+if assignment not in range(1, MAX_ASSIGNMENT+1):
+    print("Invalid assignment ID.")
+    sys.exit()
+
+# SAFETY GUARD: if the grading files already exist, suspend the program to prevent in-progress CSV files from being overwritten
+# DO NOT REMOVE
+# to unlock safety guard, delete the 'AX_to_grade' folder created by the program
+try:
+    # if the files don't exist, create a folder of the form 'AX/AX_to_grade' where X is the assignment number
+    os.mkdir(f"A{assignment}/A{assignment}_to_grade")
+except: 
+    print("GRADING IN PROGRESS, FILE SUSPENDED.")
+    sys.exit()
+
+assignmentcollector = defaultdict(list)
+
+with open(f"A{assignment}/A{assignment}_rubric.json") as f:
+    rubric_json = json.load(f)
+
+the_grading_filename = [f"A{assignment}/{COURSE_NAME}_A{assignment}_submissions_for_manual_grading.csv", f"A{assignment}/{COURSE_NAME}_A{assignment}_final_submissions.csv"][GRADE_ALL_SUBMISSIONS == True]
+with open(the_grading_filename) as f:
+    lines = csv.reader(f, delimiter = ',', quotechar = '"')
+    rows = [l for l in lines]
+    keys = rows[0]
+    # switch between group assignments and individual assignments based on the CSV keys provided
+    if "group_name" in keys:
+        keyoperator = ["group_name", "qid", "instance", "points", "feedback"]
+    else:
+        keyoperator = ["uid", "qid", "instance", "points", "feedback"]
+
+    # for all submissions in the csv:
+    for row in rows[1:]:
+        if GRADE_ALL_SUBMISSIONS == True or row[keys.index("old_feedback")] == '':
+            # sort the questions of each student into a folder-specific file
+            # only require important data: uid, qid, points, feedback
+            # the dummy field with key "instance" and value 1 needs to exist in every row
+            QID_NAME = ["qid", "Question"][GRADE_ALL_SUBMISSIONS == True]
+            GROUP_NAME = ["group_name", "Group name"][GRADE_ALL_SUBMISSIONS == True]
+            UID_NAME = ["uid", "UID"][GRADE_ALL_SUBMISSIONS == True]
+            
+            # generate some precompiled feedback
+            feedback = "Rubric:</br></br>" + "</br>".join(rubric_json[row[keys.index(QID_NAME)]]["general_comment"]) + "</br></br>"
+            if GROUP_NAME in keys:
+                newrow = [row[keys.index(GROUP_NAME)], row[keys.index(QID_NAME)], 1, '', feedback]
+            else:
+                newrow = [row[keys.index(UID_NAME)], row[keys.index(QID_NAME)], 1, '', feedback]
+            
+            assignmentcollector[row[keys.index(QID_NAME)]].append(newrow)
+
+    # iterate over questions
+    for key in assignmentcollector:
+        print("RUNNING " + key)
+
+        # THIS BUILDS THE ACTUAL QUESTION-SPECIFIC FILE
+        try:
+            os.mkdir(f"A{assignment}/A{assignment}_to_grade/{key.split('/')[0]}")
+        except: pass # make an assignment-specific folder - some assignments take questions from more than one "assignment bank"
+
+        try:
+            os.mkdir(f"A{assignment}/A{assignment}_to_grade/{key}")
+        except: pass # question-specific subfolder
+
+        # THIS CONSTRUCTS THE CSV
+        with open(f"A{assignment}/A{assignment}_to_grade/{key}/{key.replace('/', '_')}_TO_GRADE.csv", "w", newline = '') as f2:
+            writer = csv.writer(f2, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL) 
+
+            # put the CSV key into the file
+            writer.writerow(keyoperator)
+            # put the students into the file
+            for row in sorted(assignmentcollector[key], key = lambda x: x[0].lower()):
+                writer.writerow(row)
+        
+        # THIS PUTS THE FILES ASSOCIATED WITH THE CSV IN THE CORRESPONDING FOLDER
+        for row in assignmentcollector[key]:
+            print("    copying " + row[0])
+            try:
+                # create a group/student-specific folder within the question subfolder
+                os.mkdir(f"A{assignment}/A{assignment}_to_grade/{key}/{row[0]}")
+            except: pass # again, skip if exists
+
+            for foldername in os.listdir(f"A{assignment}/Assignment{assignment}"):
+                amount_to_remove = [1, 2][GRADE_ALL_SUBMISSIONS == True]
+                if "_".join(foldername.split("_")[:-amount_to_remove]) == row[0]:
+                    target_assignment = foldername.split("_")[-1]
+                    for filename in os.listdir(f"A{assignment}/Assignment{assignment}/{foldername}"):
+                        if (target_assignment+"/"+filename).startswith(key):
+                            copyfile(f"A{assignment}/Assignment{assignment}/{foldername}/{filename}", f"A{assignment}/A{assignment}_to_grade/{key}/{row[0]}/{filename}")
+
+# CREATE TXT FILES WITH SUBMISSION TEXT IN THEM FOR EASY VIEWING
+for sub_assignment in os.listdir(f"A{assignment}/A{assignment}_to_grade"):
+    print(sub_assignment, "producing TXT file...")
+    for question in os.listdir(f"A{assignment}/A{assignment}_to_grade/{sub_assignment}"):
+        print(f"    {question}")
+        with open(f"A{assignment}/A{assignment}_to_grade/{sub_assignment}/{question}/{sub_assignment+'_'+question}_aggregate_submissions.txt", "w", encoding='latin-1') as f:
+            collected_cwls = []
+            for cwl in os.listdir(f"A{assignment}/A{assignment}_to_grade/{sub_assignment}/{question}"):
+                # check that this is actually a CWL folder
+                if os.path.isdir(f"A{assignment}/A{assignment}_to_grade/{sub_assignment}/{question}/{cwl}"):
+                    collected_cwls.append(cwl)
+            collected_cwls.sort(key = lambda x: x.lower()) # alphabetical sort
+            for cwl in collected_cwls:
+                f.write("* BEGIN NEW CWL or NAME ***************************************************\n")
+                f.write("***************************************************************************\n")
+                f.write("***\n")
+                f.write("*** CWL or NAME: " + cwl + "\n")
+                for codefile in os.listdir(f"A{assignment}/A{assignment}_to_grade/{sub_assignment}/{question}/{cwl}"):
+                    f.write("***\n")
+                    f.write("----- BEGIN NEW FILE ------------------------------------------------------\n")
+                    filename = "_".join(codefile.replace(question + "_", "").split("_")[1:])
+                    f.write("***\n")
+                    f.write("*** FILENAME: " + filename+" (CWL or NAME: "+ cwl+")\n")
+                    f.write("***\n")
+                    f.write("***\n")
+                    f.write("***\n")
+                    with open(f"A{assignment}/A{assignment}_to_grade/{sub_assignment}/{question}/{cwl}/{codefile}",encoding='latin-1') as f2:
+                        for line in f2:
+                            f.write(line)
+                    f.write("***\n")
+                    f.write("***\n")
+                    f.write("***\n")
